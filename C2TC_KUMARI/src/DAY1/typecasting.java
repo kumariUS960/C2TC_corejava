5167a4d9832588cfdc8759e5a24127c35e97532a
kumariUS960 <kumari.rsu@gmail.com>
2025-07-21 22:09:11 +0530

    day1 and day2

---
+++ C2TC_KUMARI/src/DAY1/typecasting.java
@@ -0,0 +1,36 @@
+package DAY1;
+
+public class typecasting {
+	public static void main(String[] args) {
+		
+		byte b=10;
+		int i=b;
+		System.out.println(i);
+		
+		float a=10f;
+		float s=a;
+		System.out.println(s);
+		
+		double c=5d;
+		double z=c;
+		System.out.println(z);
+		
+		char var1='\u00A7';
+		char y=var1;
+		System.out.println(y);
+		
+		
+		double f1= 11.23f;
+		long l = (long) f1;
+		System.out.println(l);
+		
+		byte b1=90;
+		char ch=(char) b1;
+		System.out.println(ch);
+		
+		
+		
+	}
+
+
+}

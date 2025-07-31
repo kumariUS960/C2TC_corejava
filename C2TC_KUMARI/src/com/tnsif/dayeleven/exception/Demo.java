20a2a8438c18ad86074377e083bdc2f188b64c58
kumariUS960 <kumari.rsu@gmail.com>
2025-07-31 16:36:40 +0530

    push

---
+++ C2TC_KUMARI/src/com/tnsif/dayeleven/exception/Demo.java
@@ -0,0 +1,9 @@
+package com.tnsif.dayeleven.exception;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+
+}
--- C2TC_KUMARI/src/com/tnsif/daynine/demo/StringSubclass.java
+++ C2TC_KUMARI/src/com/tnsif/daynine/demo/StringSubclass.java
@@ -3,6 +3,7 @@ package com.tnsif.daynine.demo;
 public class StringSubclass extends String {
 	public static void main(String[] args) {
 		
+		
 	}
 
 }
